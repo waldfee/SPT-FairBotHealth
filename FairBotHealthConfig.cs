6d4a02125059e60469b8f8226eecb4547b5d9d87
waldfee <8242778+waldfee@users.noreply.github.com>
2025-10-23 08:32:49 +0200

    add config support

---
+++ FairBotHealthConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace SPT_FairBotHealth;
+
+public sealed record FairBotHealthConfig
+{
+    public static FairBotHealthConfig Default => new() { GlobalMultiplier = 1 };
+    
+    public double? GlobalMultiplier { get; init; }
+    
+    public BodyParts? BodyPartMultipliers { get; init; }
+
+    public sealed record BodyParts
+    {
+        public double? Chest { get; init; }
+        public double? Head { get; init; }
+        public double? Stomach { get; init; }
+        public double? LeftLeg { get; init; }
+        public double? RightLeg { get; init; }
+        public double? LeftArm { get; init; }
+        public double? RightArm { get; init; }
+    }
+}
--- Mod.cs
+++ Mod.cs
@@ -1,9 +1,12 @@
-﻿using SPTarkov.DI.Annotations;
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Services;
+using SPTarkov.Server.Core.Utils;
 
 namespace SPT_FairBotHealth;
 
@@ -23,16 +26,19 @@ public sealed record ModMetadata : AbstractModMetadata
 }
 
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 999)]
-public sealed class BossFairnessMod(ISptLogger<BossFairnessMod> logger, DatabaseService databaseService) : IOnLoad
+public sealed class BossFairnessMod(
+    ISptLogger<BossFairnessMod> logger,
+    DatabaseService databaseService,
+    ModHelper modHelper,
+    JsonUtil jsonUtil) : IOnLoad
 {
     private const string LogPrefix = "[FairBotHealth] ";
     
-    public Task OnLoad()
+    public async Task OnLoad()
     {
         try
         {
-            SetBotHealth();
-            return Task.CompletedTask;
+            await SetBotHealth();
         }
         catch (Exception ex)
         {
@@ -41,10 +47,11 @@ public sealed class BossFairnessMod(ISptLogger<BossFairnessMod> logger, Database
         }
     }
 
-    private void SetBotHealth()
+    private async Task SetBotHealth()
     {
         var playerHealth = databaseService.GetGlobals().Configuration.Health.ProfileHealthSettings.BodyPartsSettings;
-
+        var config = await GetConfig();
+        
         foreach (var (botTypeName, botType) in databaseService.GetBots().Types)
         {
             if (botType == null)
@@ -54,17 +61,21 @@ public sealed class BossFairnessMod(ISptLogger<BossFairnessMod> logger, Database
             
             foreach (var bodyPart in botType.BotHealth.BodyParts)
             {
-                SetAdjustedValue(bodyPart.Chest, playerHealth.Chest.Maximum);
-                SetAdjustedValue(bodyPart.Head, playerHealth.Head.Maximum);
-                SetAdjustedValue(bodyPart.Stomach, playerHealth.Stomach.Maximum);
-                SetAdjustedValue(bodyPart.LeftLeg, playerHealth.LeftLeg.Maximum);
-                SetAdjustedValue(bodyPart.RightLeg, playerHealth.RightLeg.Maximum);
-                SetAdjustedValue(bodyPart.LeftArm, playerHealth.LeftArm.Maximum);
-                SetAdjustedValue(bodyPart.RightArm, playerHealth.RightArm.Maximum);
+                SetAdjustedValue(bodyPart.Chest, playerHealth.Chest.Maximum * config.BodyPartMultipliers?.Chest ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.Head, playerHealth.Head.Maximum * config.BodyPartMultipliers?.Head ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.Stomach, playerHealth.Stomach.Maximum * config.BodyPartMultipliers?.Stomach ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.LeftLeg, playerHealth.LeftLeg.Maximum * config.BodyPartMultipliers?.LeftLeg ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.RightLeg, playerHealth.RightLeg.Maximum * config.BodyPartMultipliers?.RightLeg ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.LeftArm, playerHealth.LeftArm.Maximum * config.BodyPartMultipliers?.LeftArm ?? config.GlobalMultiplier ?? 1);
+                SetAdjustedValue(bodyPart.RightArm, playerHealth.RightArm.Maximum * config.BodyPartMultipliers?.RightArm ?? config.GlobalMultiplier ?? 1);
             }
         }
         
-        logger.Success($"{LogPrefix}Finished adjusting bot health.");
+        if (config.GlobalMultiplier != null)
+            logger.Success($"{LogPrefix}Finished adjusting bot health with {config.GlobalMultiplier:F1} multiplier.");
+        else if (config.BodyPartMultipliers != null)
+            logger.Success(
+                $"{LogPrefix}Finished adjusting bot health with {config.BodyPartMultipliers.Head:F1}, {config.BodyPartMultipliers.Chest:F1}, {config.BodyPartMultipliers.Stomach:F1}, {config.BodyPartMultipliers.LeftArm:F1}, {config.BodyPartMultipliers.RightArm:F1}, {config.BodyPartMultipliers.LeftLeg:F1}, {config.BodyPartMultipliers.RightLeg:F1} multipliers.");
 
         void SetAdjustedValue(MinMax<double> botHealth, double clampAt)
         {
@@ -72,5 +83,48 @@ public sealed class BossFairnessMod(ISptLogger<BossFairnessMod> logger, Database
             botHealth.Min = newValue;
             botHealth.Max = newValue;
         }
+    }
+
+    private async Task<FairBotHealthConfig> GetConfig()
+    {
+        var modPath = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        var configPath = Path.Combine(modPath, "Config", "config.jsonc");
+        var config = await jsonUtil.DeserializeFromFileAsync<FairBotHealthConfig>(configPath);
+        
+        if (config != null)
+        {
+            if (config.GlobalMultiplier != null &&
+                (config.BodyPartMultipliers?.Chest != null
+                 || config.BodyPartMultipliers?.RightArm != null
+                 || config.BodyPartMultipliers?.LeftArm != null
+                 || config.BodyPartMultipliers?.LeftLeg != null
+                 || config.BodyPartMultipliers?.RightLeg != null
+                 || config.BodyPartMultipliers?.Head != null
+                 || config.BodyPartMultipliers?.Stomach != null))
+            {
+                logger.Warning(
+                    $"{LogPrefix}Both {nameof(FairBotHealthConfig.GlobalMultiplier)} and {nameof(FairBotHealthConfig.BodyPartMultipliers)} configured, choose one. Using fallback default.");
+                return FairBotHealthConfig.Default;
+            }
+
+            if (config.GlobalMultiplier == null &&
+                (config.BodyPartMultipliers?.Chest == null
+                 || config.BodyPartMultipliers?.RightArm == null
+                 || config.BodyPartMultipliers?.LeftArm == null
+                 || config.BodyPartMultipliers?.LeftLeg == null
+                 || config.BodyPartMultipliers?.RightLeg == null
+                 || config.BodyPartMultipliers?.Head == null
+                 || config.BodyPartMultipliers?.Stomach == null))
+            {
+                logger.Warning(
+                    $"{LogPrefix}Both {nameof(FairBotHealthConfig.GlobalMultiplier)} and {nameof(FairBotHealthConfig.BodyPartMultipliers)} are missing in the config, choose one. Using fallback default.");
+                return FairBotHealthConfig.Default;
+            }
+            
+            return config;
+        }
+
+        logger.Warning($"{LogPrefix}No config file found, fallback to defaults.");
+        return FairBotHealthConfig.Default;
     }
 }
