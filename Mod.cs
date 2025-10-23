27393ca12e8d688386f4bffc42a57f2b2fa86992
waldfee <8242778+waldfee@users.noreply.github.com>
2025-10-23 08:35:20 +0200

    bump version

--- Mod.cs
+++ Mod.cs
@@ -16,7 +16,7 @@ public sealed record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Fair Bot Health";
     public override string Author { get; init; } = "waldfee";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
